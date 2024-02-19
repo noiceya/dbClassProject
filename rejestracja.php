22a865d65f1a25c6613f43de971049f5b098ff14
uposinski <117991942+uposinski@users.noreply.github.com>
2024-02-19 09:57:38 +0100

    Add files via upload
    STRONA LOGOWANIA I REJESTRACJI POZDRAWIAM :D

---
+++ rejestracja.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="rej.css">
+    <script src="rej.js"></script>
+    <title>Przepisy.pl</title>
+</head>
+<body>
+    <div class="form-structor">
+        <div class="signup">
+            <h2 class="form-title" id="signup"><span>lub</span>Zarejestruj się!</h2>
+            <div class="form-holder">
+                <input type="text" class="input" placeholder="Name" />
+                <input type="email" class="input" placeholder="Email" />
+                <input type="password" class="input" placeholder="Password" />
+            </div>
+            <button class="submit-btn">Utwórz konto</button>
+        </div>
+        <div class="login slide-up">
+            <div class="center">
+                <h2 class="form-title" id="login"><span>lub</span>Zaloguj się</h2>
+                <div class="form-holder">
+                    <input type="email" class="input" placeholder="Email" />
+                    <input type="password" class="input" placeholder="Password" />
+                </div>
+                <button class="submit-btn">Log in</button>
+            </div>
+        </div>
+</div>
+</body>
+</html>
